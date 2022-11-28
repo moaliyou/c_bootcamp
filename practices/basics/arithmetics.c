634662a3ceebe3c7421d97be53a60b6214239f70
moalyou <moaliyou47@gmail.com>
2022-11-28 21:24:07 +0300

    testing arithmetic operations in c

---
+++ practices/basics/arithmetics.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int num_1;
+    int num_2;
+
+    puts("Enter first number: ");
+    scanf("%d", &num_1);
+
+    puts("Enter second number: ");
+    scanf("%d", &num_2);
+
+    int sum = num_1 + num_2;
+
+    printf("sum is %d\n", sum);
+
+    return 0;
+}
